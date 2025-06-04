2090de6aae4a02a676ba13a0df28b242948fd297
Adrian Jacobo <adrian.jacobo@wallia.io>
2025-06-04 15:17:24 -0600

    Se agrega segunda corrección del instructor

--- api_ecommerce/router/User.js
+++ api_ecommerce/router/User.js
@@ -11,6 +11,7 @@ const router = routerx();
 router.post("/register",usercontroller.register);
 router.put("/update",usercontroller.update);
 router.get("/list",auth.verifyAdmin,usercontroller.list);
+router.post("/register_admin",auth.verifyAdmin,usercontroller.register_admin);
 router.post("/login",usercontroller.login);
 router.post("/login_admin",usercontroller.login_admin);
 router.delete("/delete",usercontroller.remove);
